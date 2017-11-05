efdc5cfe6b8bf7505e37e5526a7719b614b5db44
blr-Oliver <vasily.liaskovsky@gmail.com>
2017-11-05 18:30:47 +0300

    undefined CSS transition doesn't swallow `done` callback anymore

--- src/ng-slide-animations.js
+++ src/ng-slide-animations.js
@@ -1,16 +1,34 @@
 angular.module('ngSlideAnimations', ['ngAnimate']).factory('slideAnimations', ['$window', function($window){
+  var transitionProperty = getPrefixed('transitionProperty');
+  var transitionDuration = getPrefixed('transitionDuration')
 
   var CURRENT = 'current';
+
+  function getPrefixed(prop){
+    //based on https://stackoverflow.com/questions/7264899/detect-css-transitions-using-javascript-and-without-modernizr#answer-13081497
+    var style = $document[0].createElement('p').style, prefixes = ['ms', 'O', 'Moz', 'Webkit'];
+    if(style[prop] == '')
+      return prop;
+    prop = prop.charAt(0).toUpperCase() + prop.substr(1);
+    for (var i = prefixes.length; i--;)
+      if(typeof (style[prefixes[i] + prop]) === 'string')
+        return (prefixes[i] + prop);
+  }
+
   function animateSizeAndReset(element, dimension, from, to, done){
-    var oldSize = element[0].style[dimension];
-    if(from == CURRENT)
-      from = currentSize(element, dimension);
-    if(to == CURRENT)
-      to = currentSize(element, dimension);
-    element.css(dimension, from);
-    element[0].offsetHeight; //force repaint
-    element.css(dimension, to);
-    installSizeTransitionFinisher(element, dimension, oldSize, done);
+    if(hasTransition(element, dimension)){
+      if(from == CURRENT)
+        from = currentSize(element, dimension);
+      if(to == CURRENT)
+        to = currentSize(element, dimension);
+      var oldSize = element[0].style[dimension];
+      var oldDisplay = element[0].style.display;
+      element.css('display', 'none').css(dimension, from);
+      installSizeTransitionFinisher(forceRepaint(element), dimension, oldSize, done);
+      forceRepaint(element.css('display', oldDisplay)).css(dimension, to);
+    }else{
+      done && done();
+    }
   }
 
   function currentSize(element, dimension){
@@ -26,6 +44,20 @@ angular.module('ngSlideAnimations', ['ngAnimate']).factory('slideAnimations', ['
         done && done();
       }
     });
+  }
+
+  function hasTransition(element, property){
+    var style = $window.getComputedStyle(element[0]);
+    var propList = style[transitionProperty];
+    if(propList != '' && propList != 'none' && (propList === 'all' || !!~propList.split(/\s*,\s*/).indexOf(property))){
+      return parseFloat(style[transitionDuration]) > 0;
+    }else
+      return false;
+  }
+
+  function forceRepaint(element){
+    element[0].offsetHeight;
+    return element;
   }
 
   return {
